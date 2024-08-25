ef4e86f5e23507a7ce8e9c18ee64cbb5585761af
Rajveer Yadav <51945519+ray-27@users.noreply.github.com>
2024-08-25 22:35:31 +0530

    ola ready

--- main.go
+++ main.go
@@ -14,15 +14,18 @@ func setupRouter() *gin.Engine {
 
 	// Define the route using the handler from another file
 	router.GET("/", handlers.Home)
-	router.GET("/about",handlers.About)
+	router.GET("/about", handlers.About)
 
 	return router
 }
 
 func main() {
+
+
 	router := setupRouter()
 
 	// Start serving the application
 	println("localhost:8080")
-	router.Run(":8080") // listens on :8080 by default
+	router.Run("localhost:8080") // listens on :8080 by default
+
 }
